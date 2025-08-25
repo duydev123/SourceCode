fcc7177d10a54667e8704a263ecd38d1a984b6e5
duydev123 <duyhan44@gmail.com>
2025-08-25 14:31:36 +0700

    Add files via upload

--- GameMain/main.cpp
+++ GameMain/main.cpp
@@ -1,24 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include "../ShapedGames/PongGame/Pong.h"
+#include <iostream>
 //
 int main(){
     // tạo window
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Multi Game", sf::State::Fullscreen);
     // game tạm thời
-    PongGame game(window);
+    Pong::PongGame game(window);
     // vòng lập chính 
     while(window.isOpen()){
+        window.setFramerateLimit(60);
         // event cơ bản của sfml
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+            game.EventHandle(event);
         }
-        // màu nền
         window.clear(sf::Color::White);
+        game.Update();
         // hàm update của gam đã đóng gói
-        game.Update(window);
         // cái luôn cần thiết để chạy
         window.display();
     }
